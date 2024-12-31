2c1d513db7c2b9b591f004edfed76aa051f63a86
falcon <shastrin22@gmail.com>
2024-12-31 10:24:40 +0530

    Time: 9 ms (50.18%) | Memory: 7.7 MB (54.11%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n<=1){
+        return n;
+    }
+   
+int first=fib(n-1);
+int last =fib(n-2);
+return first + last;
+        
+    }
+   
+
+};
